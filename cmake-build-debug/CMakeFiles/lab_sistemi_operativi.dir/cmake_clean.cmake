8d25cac9a0cbe16cac550e0854aa618d02b336a0
stefano <s.manno@live.it>
2019-11-12 17:19:01 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/lab_sistemi_operativi.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab_sistemi_operativi.dir/esProva.c.obj"
+  "CMakeFiles/lab_sistemi_operativi.dir/main.c.obj"
+  "lab_sistemi_operativi.exe"
+  "lab_sistemi_operativi.exe.manifest"
+  "lab_sistemi_operativi.pdb"
+  "liblab_sistemi_operativi.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab_sistemi_operativi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
